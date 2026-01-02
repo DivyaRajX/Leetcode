4386569313bdc3364f1d5c3d97423dd3de5aec40
Divya Raj Varshney <varshneydivya251707@gmail.com>
2026-01-02 22:16:04 +0530

    Time: 0 ms (100%), Space: 47.8 MB (6.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,17 +1,8 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i: nums){
-            if(!hm.containsKey(i)){
-                hm.put(i, 1);
-            }
-            else{
-                hm.put(i, hm.get(i)+1);
-            }
-        }
-
-        for(int i: hm.keySet()){
-            if(hm.get(i) > 1) return i;
-        }return -1;
+        //pigeon hole principle
+        for(int i=0; i<nums.length-2; i++){
+            if(nums[i]==nums[i+1] || nums[i]==nums[i+2]) return nums[i];
+        }return nums[nums.length-1];
     }
 }
