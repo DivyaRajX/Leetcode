825065872117248e8ecddbf791fca836e5c8e57d
Divya Raj Varshney <varshneydivya251707@gmail.com>
2026-02-19 00:02:52 +0530

    Time: 0 ms (100%), Space: 44.4 MB (81.87%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,32 +15,11 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        return iterTraversal(root);
-    }
-
-    static int iterTraversal(TreeNode root){
         if(root == null) return 0;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        int l = 0;
-        q.offer(root);
-        while(!q.isEmpty()){
-            int levelSize = q.size();
-            for(int i=0; i<levelSize; i++){
-                TreeNode curr = q.poll();
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
 
-                if(curr.left !=  null) q.offer(curr.left);
-                if(curr.right != null) q.offer(curr.right);
-            }
-            l++;
-        }return l;
-    }
-    static int recursiveTraversal(TreeNode root){
-        if(root == null) return 0;
-
-        int l = recursiveTraversal(root.left);
-        int r = recursiveTraversal(root.right);
-
-        return Math.max(l,r)+1;
+        return 1+Math.max(left, right);
     }
 }
