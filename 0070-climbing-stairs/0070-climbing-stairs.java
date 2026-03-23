3c09d2739ce3798f10775583a97be7a9ba10749e
Divya Raj Varshney <varshneydivya251707@gmail.com>
2026-03-23 10:37:05 +0530

    Time: 0 ms (100%), Space: 42 MB (82.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
+        int dp[] = new int[n+1];
+        return topDwn(n, dp);
     }
 
-    int helper(int n, int[] dp){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int res = helper(n-1, dp)+helper(n-2, dp);
-        return dp[n] = res;
+    int topDwn(int n, int[] dp){
+        if(n == 1 || n == 2) return n;
+        if(dp[n] != 0) return dp[n];
+
+        int f = topDwn(n-1, dp);
+        int s = topDwn(n-2, dp);
+
+        return dp[n] = f+s;
     }
 }
