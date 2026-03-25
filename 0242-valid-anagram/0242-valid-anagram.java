f76389936d132e8e97d2252265a3eac6fc68e35d
Divya Raj Varshney <varshneydivya251707@gmail.com>
2026-03-25 20:11:17 +0530

    Time: 28 ms (5.51%), Space: 47 MB (6.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!= t.length()) return false;
-        HashMap<Character, Integer> map= new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(!map.containsKey(ch)) map.put(ch, 1);
-            else map.put(ch, map.get(ch)+1);
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2  = new HashMap<>();
+        for(int i = 0 ; i<s.length(); i++){
+            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0)+1);
+            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0)+1);
         }
+        for(char ch : map2.keySet()){
+            if(!map1.containsKey(ch) || !map1.get(ch).equals(map2.get(ch))){
+                System.out.println(map2.get(ch));
+                return false;
 
-        for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch)) return false;
-            else if(map.containsKey(ch) && map.get(ch) > 0) map.put(ch, map.get(ch)-1);
-            else if(map.get(ch) <= 0) return false;
+            }
         }
         return true;
     }
