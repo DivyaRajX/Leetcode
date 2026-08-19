fada26fca734362be8b3f08ea04c669870b29d72
Divya Raj Varshney <varshneydivya251707@gmail.com>
2026-08-20 05:12:49 +0530

    Time: 606 ms (5.01%), Space: 51.7 MB (5.57%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int n = s.length();
+        int[] pre = new int[n], suf = new int[n];
+        String ans = "";
+
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) == '(') {
+                if(cnt < 0) cnt = 0;
+                pre[i] = ++cnt;
+            }
+            else if(s.charAt(i) == ')') pre[i] = --cnt;
+        }
+        cnt = 0;
+        for(int i=n-1; i>=0; i--){
+            if(s.charAt(i) == ')') {
+                if(cnt < 0) cnt = 0;
+                suf[i] = ++cnt;
+            }
+            else if(s.charAt(i) == '(') suf[i] = --cnt;
+        }
+        System.out.println(Arrays.toString(pre));
+        System.out.println(Arrays.toString(suf));
+        for(int i=0; i<n; i++){
+            if(pre[i] >= 0 && suf[i] >= 0) ans += s.charAt(i);
+        }
+        return ans;
+    }
+}
